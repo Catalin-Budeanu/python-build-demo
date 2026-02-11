f7d281b9da04045d2b5fa9fa3c9b68564437d734
Budeanu Catalin <catalinbudeanu12@gmail.com>
2026-02-11 20:27:31 +0200

    resolve test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,6 +4,3 @@ def test_calculate_total():
     assert calculate_total([10, 15, 25]) == 50
     assert calculate_total([]) == 0
 
-def test_calculate_total():
-    assert calculate_total([10, 15, 25]) == 60
-    assert calculate_total([]) == 0
